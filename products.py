715c3cfa7d658817524eb1e8a52c3871920a736a
shynuie <yuhsien7831@icloud.com>
2020-07-06 18:21:46 +0800

    update products.py

--- products.py
+++ products.py
@@ -7,5 +7,7 @@ while True:
     if price == 'q':
         break
     products.append([name, price])
-with open('products.csv',w) as f:
-    for line 
+with open('products.csv', 'w', encoding = 'utf-8') as f:
+    f.write('商品,价格\n')
+    for p in products:
+        f.write(p[0] + ',' + p[1] + '\n')
