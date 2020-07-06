d508c0dfc99b2c8bbd5e0828bc422590d0f0b137
shynuie <yuhsien7831@icloud.com>
2020-07-07 07:36:39 +0800

    adding checking file function

--- products.py
+++ products.py
@@ -1,4 +1,17 @@
+import os
+
 products = []
+if os.path.isfile('products.csv'):
+    #读取档案
+    with open('products.csv', 'r', encoding = 'utf-8') as f:
+        for line in f:
+            if '商品,价格' in line:
+                continue 
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    print(products)
+
+#输入资料
 while True:
     name = input('Pls enter name of item (q to quit):')
     if name == 'q':
@@ -7,6 +20,7 @@ while True:
     if price == 'q':
         break
     products.append([name, price])
+#写入档案
 with open('products.csv', 'w', encoding = 'utf-8') as f:
     f.write('商品,价格\n')
     for p in products:
