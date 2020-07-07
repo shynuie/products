dd4f2303ab72531dcdaed59139c1937d0e4d328c
shynuie <yuhsien7831@icloud.com>
2020-07-07 13:50:49 +0800

    refactor

--- products.py
+++ products.py
@@ -1,27 +1,52 @@
 import os
-
 products = []
-if os.path.isfile('products.csv'):
-    #读取档案
-    with open('products.csv', 'r', encoding = 'utf-8') as f:
-        for line in f:
-            if '商品,价格' in line:
-                continue 
-            name, price = line.strip().split(',')
-            products.append([name, price])
-    print(products)
+filename = ''
+def read_file():        #读取资料(回传'q'或者'n'或者products)
+    while True:
+        global filename
+        filename = input('Pls enter name of the file to read:')
+        if filename == 'q':
+            return filename, products
+            break
+        elif filename == 'n':
+            return filename, products
+            break
+        elif os.path.isfile(filename):
+            with open(filename, 'r', encoding = 'utf-8') as f:
+                for line in f:
+                    if '商品,价格' in line:
+                        continue 
+                    name, price = line.strip().split(',')
+                    products.append([name, price])
+            print('成功读取，以下为读取到的资料：\n' , products)
+            return filename, products
+            break
+        else:
+            print('File does not exist, pls enter again or type "n" for new file, "q" for quit\n')
 
-#输入资料
-while True:
-    name = input('Pls enter name of item (q to quit):')
-    if name == 'q':
-        break
-    price = input('pls enter price of item (q to quit):')
-    if price == 'q':
-        break
-    products.append([name, price])
-#写入档案
-with open('products.csv', 'w', encoding = 'utf-8') as f:
-    f.write('商品,价格\n')
-    for p in products:
-        f.write(p[0] + ',' + p[1] + '\n')
+def input_data():   #输入资料
+    while True:
+        name = input('Pls enter name of item (q to quit):')
+        if name == 'q':
+            break
+        price = input('pls enter price of item (q to quit):')
+        if price == 'q':
+            break
+        products.append([name, price])
+    return products
+def saving():   #写入档案
+    name = input('Pls name the file, "s" for the same:')
+    if name == 's':
+        global filename
+        name = filename
+    with open(name, 'w', encoding = 'utf-8') as f:
+        f.write('商品,价格\n')
+        for p in products:
+            f.write(p[0] + ',' + p[1] + '\n')
+def main():
+    filename, products = read_file()
+    print(filename)
+    if filename != 'q':
+        products = input_data()
+        saving()
+main()
